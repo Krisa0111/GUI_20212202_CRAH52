75a586f0955279a0f0d7d73749cfcbeb847a3d61
Krisa0111 <pingipongi01@gmail.com>
2022-03-31 18:19:21 +0200

    Basics added

--- Game/Renderer/Display.cs
+++ Game/Renderer/Display.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Game.Model
+namespace Game.Renderer
 {
-    class GameModel
+    class Display
     {
     }
 }
--- Game/Renderer/MainMenuDisplay.cs
+++ Game/Renderer/MainMenuDisplay.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Game.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,6 +13,17 @@ namespace Game
 {
     public class MainMenuDisplay : FrameworkElement
     {
+        IGameModell model;
+        Size size;
+        public void Resize(Size size)
+        {
+            this.size = size;
+        }
+        public void SetupModel(IGameModell gameModell)
+        {
+            this.model = gameModell;
+        }
+
         public Brush BackGroundBrush
         {
             get
@@ -22,6 +34,10 @@ namespace Game
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
+            if (model != null && size.Width > 50 && size.Height>50)
+            {
+
+            }
             if (ActualWidth >0 && ActualHeight >0)
             {
                 drawingContext.DrawRectangle(BackGroundBrush, null, new Rect(0, 0, ActualWidth, ActualHeight));
