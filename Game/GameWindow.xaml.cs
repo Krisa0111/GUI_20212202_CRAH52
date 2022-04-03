99a75380dbd5f35451b395aa80c21ca9af799256
naszly <naszaly.k@gmail.com>
2022-04-03 13:46:33 +0200

    game dispay

--- Game/GameWindow.xaml.cs
+++ Game/GameWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Game.Graphics;
+using Game.Renderer;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
 using OpenTK.Wpf;
@@ -15,16 +16,7 @@ namespace Game
     /// 
     public partial class GameWindow : Window
     {
-        private Shader shader;
-        private Shader playerShader;
-
-        private Camera camera;
-
-        private StaticMesh<VertexColorTextureUV> mesh1;
-        private StaticMesh<VertexTextureUV> playerMesh;
-
-        private Texture texture1;
-        private Texture playerTexture;
+        GameDisplay gameDisplay;
 
         public GameWindow()
         {
@@ -37,85 +29,19 @@ namespace Game
             };
             OpenTkControl.Start(settings);
 
-            GL.Enable(EnableCap.DepthTest);
-            
-            shader = new Shader("Shaders/simplev.glsl", "Shaders/colortexturef.glsl");
-            playerShader = new Shader("Shaders/playerv.glsl", "Shaders/texturef.glsl");
-            texture1 = Texture.LoadFromFile("Images/metalbox.png");
-            playerTexture = Texture.LoadFromFile("Images/running.png");
-
-            camera = new Camera(Vector3.UnitZ * 1, (float)(ActualWidth / ActualHeight));
-
-            List<VertexColorTextureUV> vertices1 = new List<VertexColorTextureUV>();
-
-            vertices1.Add(new VertexColorTextureUV(new Vector3(-0.5f, -0.5f, 0.0f), new Vector3(0,0,1), new Vector3(0,0,1), new Vector2(0,0)));
-            vertices1.Add(new VertexColorTextureUV(new Vector3(0.5f, -0.5f, 0.0f), new Vector3(0,0,1), new Vector3(0,1,0), new Vector2(1,0)));
-            vertices1.Add(new VertexColorTextureUV(new Vector3(0.0f, 0.5f, 0.0f), new Vector3(0,0,1), new Vector3(1,0,0), new Vector2(.5f,1)));
-
-            mesh1 = new StaticMesh<VertexColorTextureUV>(vertices1);
-
-            List<VertexTextureUV> vertices2 = new List<VertexTextureUV>();
-
-            vertices2.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(0, 0)));
-            vertices2.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(0, 1)));
-            vertices2.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(1, 1)));
-            vertices2.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(0, 0)));
-            vertices2.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(1, 1)));
-            vertices2.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(1, 0)));
-
-            playerMesh = new StaticMesh<VertexTextureUV>(vertices2);
-
+            gameDisplay = new GameDisplay((float)ActualWidth, (float)ActualHeight);
         }
 
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
             base.OnRenderSizeChanged(sizeInfo);
-            camera.AspectRatio = (float)(ActualWidth / ActualHeight);
+            gameDisplay.Resize((float)sizeInfo.NewSize.Width, (float)sizeInfo.NewSize.Height);
         }
 
-        int ang = 0;
-        int x = 1;
-        int y = 1;
+        
         private void OpenTkControl_OnRender(TimeSpan delta)
         {
-            GL.ClearColor(Color4.Blue);
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
-            ang++;
-
-            shader.Use();
-            var model = Matrix4.Identity * Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(ang)) * Matrix4.CreateTranslation(Vector3.UnitX);
-            shader.SetMatrix4("model", model);
-            shader.SetMatrix4("view", camera.GetViewMatrix());
-            shader.SetMatrix4("projection", camera.GetProjectionMatrix());
-
-            texture1.Use(TextureUnit.Texture0);
-
-            mesh1.Draw();
-
-
-            GL.Enable(EnableCap.Blend);
-            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusConstantAlpha);
-
-            playerShader.Use();
-            playerShader.SetMatrix4("model", Matrix4.Identity);
-            playerShader.SetMatrix4("view", camera.GetViewMatrix());
-            playerShader.SetMatrix4("projection", camera.GetProjectionMatrix());
-            playerShader.SetVector2("textureID", new Vector2(x, y));
-            x++;
-            if (x > 16)
-            {
-                x = 1;
-                y++;
-                if (y > 8) y = 1;
-            }
-
-            playerTexture.Use(TextureUnit.Texture0);
-
-            playerMesh.Draw();
-
-            GL.Disable(EnableCap.Blend);
-
+            gameDisplay.Render();
 
             var code = GL.GetError();
             while (code != ErrorCode.NoError)
--- Game/Renderer/GameDisplay.cs
+++ Game/Renderer/GameDisplay.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Game.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,177 @@ namespace Game.Renderer
 {
     internal class GameDisplay
     {
-        
+        private Shader gameItemShader;
+        private Shader playerShader;
+
+        private Camera mainCamera;
+
+        private StaticMesh<VertexTextureUV> squareMesh;
+        private StaticMesh<VertexTextureUV> floorMesh;
+        private StaticMesh<VertexTextureUV> boxMesh;
+
+        private Texture floorTexture;
+        private Texture boxTexture;
+        private Texture playerTexture;
+        private Vector2 playerAtlasSize;
+        private Vector2 playerAtlasPos;
+
+        public GameDisplay(float width, float height)
+        {
+            GL.Enable(EnableCap.DepthTest);
+
+            gameItemShader = new Shader("Shaders/simplev.glsl", "Shaders/texturef.glsl");
+            playerShader = new Shader("Shaders/playerv.glsl", "Shaders/texturef.glsl");
+            playerTexture = Texture.LoadFromFile("Images/running3.png");
+            floorTexture = Texture.LoadFromFile("Images/textureStone.png");
+            boxTexture = Texture.LoadFromFile("Images/metalbox.png");
+
+            mainCamera = new Camera(new Vector3(0.0f, 1.5f, 1.5f), width / height);
+            mainCamera.Pitch = -15f;
+
+            squareMesh = new StaticMesh<VertexTextureUV>(generateSquareMesh());
+
+            squareMesh.AttachTexture(playerTexture);
+
+            playerAtlasSize = new Vector2(16, 3);
+            playerAtlasPos = new Vector2(1, 1);
+
+            playerShader.Use();
+            playerShader.SetVector2("atlasSize", playerAtlasSize);
+
+            floorMesh = new StaticMesh<VertexTextureUV>(generateFloorMesh());
+
+            floorMesh.AttachTexture(floorTexture);
+
+            boxMesh = new StaticMesh<VertexTextureUV>(generateBoxMesh());
+
+            boxMesh.AttachTexture(boxTexture);
+
+            //GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+        }
+
+        public void Resize(float width, float height)
+        {
+            mainCamera.AspectRatio = width / height;
+        }
+
+        float x = 0;
+        public void Render()
+        {
+            GL.ClearColor(Color4.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // render floor
+
+            mainCamera.SetMatrices(gameItemShader);
+
+            gameItemShader.Use();
+            gameItemShader.SetVector2("uvOffset", new Vector2(0, x+=0.02f));
+
+            floorMesh.Draw(gameItemShader, Matrix4.Identity);
+
+            // render game items
+
+            gameItemShader.SetVector2("uvOffset", new Vector2(0, 0));
+
+            boxMesh.Draw(gameItemShader, Matrix4.Identity * Matrix4.CreateTranslation(new Vector3(1,0.0f,-1)));
+
+
+            // render player
+
+            GL.Enable(EnableCap.Blend); // enable transparency
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+            mainCamera.SetMatrices(playerShader);
+
+            playerShader.Use();
+            playerShader.SetVector2("atlasXY", playerAtlasPos);
+            playerAtlasPos.X++;
+            if (playerAtlasPos.X > playerAtlasSize.X)
+            {
+                playerAtlasPos.X = 1;
+                playerAtlasPos.Y++;
+                if (playerAtlasPos.Y > playerAtlasSize.Y) playerAtlasPos.Y = 1;
+            }
+
+            squareMesh.Draw(playerShader, Matrix4.Identity);
+
+            GL.Disable(EnableCap.Blend);
+        }
+
+        private List<VertexTextureUV> generateSquareMesh()
+        {
+            List<VertexTextureUV> square = new List<VertexTextureUV>();
+            square.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(0, 0)));
+            square.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(0, 1)));
+            square.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(1, 1)));
+            square.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(0, 0)));
+            square.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(1, 1)));
+            square.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, 0.0f), new Vector3(0, 0, 1), new Vector2(1, 0)));
+            return square;
+        }
+
+        private List<VertexTextureUV> generateFloorMesh()
+        {
+            List<VertexTextureUV> floor = new List<VertexTextureUV>();
+
+            floor.Add(new VertexTextureUV(new Vector3(-1.5f, -0.5f, 1.0f), new Vector3(0, 1, 0), new Vector2(0, 0)));
+            floor.Add(new VertexTextureUV(new Vector3(1.5f, -0.5f, -299.0f), new Vector3(0, 1, 0), new Vector2(1, 100)));
+            floor.Add(new VertexTextureUV(new Vector3(-1.5f, -0.5f, -299.0f), new Vector3(0, 1, 0), new Vector2(0, 100)));
+            floor.Add(new VertexTextureUV(new Vector3(-1.5f, -0.5f, 1.0f), new Vector3(0, 1, 0), new Vector2(0, 0)));
+            floor.Add(new VertexTextureUV(new Vector3(1.5f, -0.5f, 1.0f), new Vector3(0, 1, 0), new Vector2(1, 0)));
+            floor.Add(new VertexTextureUV(new Vector3(1.5f, -0.5f, -299.0f), new Vector3(0, 0, 0), new Vector2(1, 100)));
+
+            return floor;
+        }
+
+        private List<VertexTextureUV> generateBoxMesh()
+        {
+            List<VertexTextureUV> box = new List<VertexTextureUV>();
+
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(0.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(1.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(1.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(1.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(0.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(0.0f, 0.0f)));
+
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(0.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(1.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(1.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(1.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(0.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(0.0f, 0.0f)));
+
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(1.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(1.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(0.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(0.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(0.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(1.0f, 0.0f)));
+
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(1.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(1.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(0.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(0.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(0.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(1.0f, 0.0f)));
+
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(0.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(1.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(1.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(1.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(0.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(0.0f, 1.0f)));
+
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, -0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 1.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f, 0.0f)));
+            box.Add(new VertexTextureUV(new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f, 1.0f)));
+
+            return box;
+        }
     }
 }
