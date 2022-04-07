d3a256ebe52575bb0c155ca93bd9917ded46deb8
Krisa0111 <pingipongi01@gmail.com>
2022-04-07 17:43:17 +0200

    Added items

---
+++ Game/Modell/Accelerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace Game.Model
+{
+    public class Accelerator
+    {
+        public System.Drawing.Point Center { get; set; }
+        static Random r = new Random();
+        public Vector Speed { get; set; }
+        private int Randomizer(int min, int max)
+        {
+            int rnd = 0;
+            do
+            {
+                rnd = r.Next(min, max + 1);
+            } while (rnd == 0);
+            return rnd;
+        }
+        public Accelerator(int xPos, int yPos)
+        {
+            xPos = r.Next(-5, 5);
+            yPos = r.Next(-5, 5);
+            Center = new System.Drawing.Point(xPos, yPos);
+
+        }
+    }
+}
--- Game/Modell/IGameModell.cs
+++ Game/Modell/IGameModell.cs
@@ -8,5 +8,16 @@ namespace Game.Model
 {
     public interface IGameModell
     {
+        event EventHandler Changed; // ide kene feliratkoztatni a renderert
+        List<Decelerator> Decelerators { get; set; }
+        List<Accelerator> Accelerators { get; set; }
+        List<BluePortal> BluePortals { get; set; }
+        List<RedPortal> RedPortals { get; set; }
+        List<PlusLife> PlusLifes { get; set; }
+        List<PremiumPortal> PremiumPortals { get; set; }
+        List<RandomItem> RandomItems { get; set; }
+        List<Skull> Skulls { get; set; }
+        List<Box> Boxes { get; set; }
+
     }
 }
