361c415ae520b045e5baf825274def6a86a65110
naszly <naszaly.k@gmail.com>
2022-05-06 20:15:46 +0200

    optimize map loading

--- Game/ResourceLoader/ChunkLoader.cs
+++ Game/ResourceLoader/ChunkLoader.cs
@@ -14,17 +14,52 @@ namespace Game.ResourceLoader
 {
     internal class ChunkLoader
     {
+        private struct EntityDef
+        {
+            public Type entityType;
+            public Vector3 position;
+
+            public EntityDef(Type entityType, Vector3 position)
+            {
+                this.entityType = entityType;
+                this.position = position;
+            }
+
+            public Entity GetEntity(Vector3 offset)
+            {
+                Entity entity = Activator.CreateInstance(entityType, args: new object[] { position + offset }) as Entity;
+                return entity;
+            }
+        }
+
         const string ENTITIES_NAME_SPACE = "Game.ViewModel.Entities";
         private static Random rnd = new Random();
-        private IList<string[]> chunks = new List<string[]>();
+        private IList<IList<EntityDef>> chunks = new List<IList<EntityDef>>();
 
         public ChunkLoader(string direcory)
         {
             string[] files = Directory.GetFiles(direcory);
 
-            for (int i = 0; i < files.Length; i++)
+            foreach (string file in files)
             {
-                chunks.Add(File.ReadAllLines(files[i]));
+                var lines = File.ReadAllLines(file);
+                List<EntityDef> entityDefs = new List<EntityDef>();
+
+                foreach (string line in lines)
+                {
+                    try
+                    {
+                        var def = ParseEntityDef(line);
+                        if (typeof(Entity).IsAssignableFrom(def.entityType))
+                            entityDefs.Add(def);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    }
+                }
+
+                chunks.Add(entityDefs);
             }
         }
 
@@ -35,21 +70,15 @@ namespace Game.ResourceLoader
             int index = rnd.Next(0, chunks.Count);
             var chunk = chunks[index];
 
-            for (int i = 0; i < chunk.Length; i++)
+            foreach (var item in chunk)
             {
-                try
-                {
-                    entities.Add(createEntity(chunk[i], offset));
-                } catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.Message);
-                }
+                entities.Add(item.GetEntity(offset));
             }
 
             return entities;
         }
 
-        private Entity createEntity(string code, Vector3 offset)
+        private EntityDef ParseEntityDef(string code)
         {
             string[] parts = code.Split(" ");
             Type type = Type.GetType(ENTITIES_NAME_SPACE + "." + parts[0]);
@@ -58,10 +87,9 @@ namespace Game.ResourceLoader
             float y = float.Parse(parts[2], CultureInfo.InvariantCulture);
             float z = float.Parse(parts[3], CultureInfo.InvariantCulture);
 
-            Vector3 pos = new Vector3(x, y, z) + offset;
-            Entity entity = Activator.CreateInstance(type, args: new object[] { pos }) as Entity;
+            EntityDef def = new EntityDef(type, new Vector3(x, y, z));
 
-            return entity;
+            return def;
         }
     }
 }
