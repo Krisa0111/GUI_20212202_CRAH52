cb40fd0e213ccc068a514766f25878dea305d7f0
naszly <naszaly.k@gmail.com>
2022-05-08 19:44:20 +0200

    add portals

---
+++ Game/ViewModel/Entities/BluePortal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.ViewModel.Entities
+{
+    internal class BluePortal : Entity
+    {
+        public BluePortal(Vector3 position)
+            : base(EntityType.BluePortal, position, ModelLoader.GetModel("Models/blue_portal.obj"))
+        { }
+    }
+}
