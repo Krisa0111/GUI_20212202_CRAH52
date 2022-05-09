e0183ed166aa6ca0a28a6d72534e59282b35eafc
naszly <naszaly.k@gmail.com>
2022-05-09 23:14:41 +0200

    add accelerator model

---
+++ Game/ViewModel/Entities/Accelerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.ViewModel.Entities
+{
+    internal class Accelerator : Entity
+    {
+        public Accelerator(Vector3 position)
+            : base(EntityType.PlusLife, position, ModelLoader.GetModel("Models/accelerator.obj"), ModelLoader.GetModel("Models/powerup_collider.obj"))
+        {
+
+        }
+    }
+}
