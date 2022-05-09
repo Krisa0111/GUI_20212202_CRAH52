6db01460874163b88b53af74d30635de4140a28f
naszly <naszaly.k@gmail.com>
2022-05-09 11:31:02 +0200

    fix streetlight

--- Game/ViewModel/Entities/StreetLight.cs
+++ Game/ViewModel/Entities/StreetLight.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Game.ResourceLoader;
 
 namespace Game.ViewModel.Entities
 {
@@ -11,7 +12,7 @@ namespace Game.ViewModel.Entities
     {
         public Vector3 LightSourceOffset { get => new Vector3(0.4f, 4.4f, 0); }
 
-        public StreetLight(Vector3 position) : base(EntityType.Obstacle, position, ModelLoader.GetModel("Models/streetlight.obj"))
+        public StreetLight(Vector3 position) : base(EntityType.Obstacle, position, ModelLoader.GetModel("Models/streetlight.obj"), Model.Empty)
         {
 
         }
